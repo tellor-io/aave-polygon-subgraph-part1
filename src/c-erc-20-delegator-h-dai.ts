9582516668b8556d26d1a2ec3617eeacd457d34e
tkernell <tpkernell@gmail.com>
2022-08-03 12:36:33 -0500

    fix dai

--- src/c-erc-20-delegator-h-dai.ts
+++ src/c-erc-20-delegator-h-dai.ts
@@ -5,7 +5,7 @@ import {
   Transfer,
 } from "../generated/schema"
 
-export function handleTransfer(event: CErc20Delegator_hDAITransferEvent): void {
+export function handleCErc20Delegator_hDAITransfer(event: CErc20Delegator_hDAITransferEvent): void {
   const id = event.transaction.hash.toHexString() + event.logIndex.toString()
   let transfer = new Transfer(id)
   transfer.contract = "0xE4e43864ea18d5E5211352a4B810383460aB7fcC"
